ae16418f6a30e141104b42e62d582466ef0ad7b3
Tech-A35 <tech-a35@Tech-A35s-MacBook-Air.local>
2018-05-09 22:14:04 +0100

    Really struggling with this

--- config.ru
+++ config.ru
@@ -2,5 +2,10 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 #Link this file with the Ruby file that has the class in
+
+
 require_relative "controllers/posts_controller.rb"
 run PostsController
+
+require_relative "controllers/users_controller.rb"
+run UsersController
--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,6 +1,36 @@
+require "sinatra"
+require "sinatra/reloader"
+
 #only camelCase thing in Ruby is class names
 class UsersController < Sinatra::Base
 
+
+
+  #$ sign sets up an ultra global variable. We need to make an array of hashes accessible from everywhere
+    $users = [
+      {
+      id: 0,
+      email: "nosugar4me@gmail.com",
+      first_name: "Sue"
+      last_name: "Skinner"
+      age: 39
+
+      },
+      {
+        id: 1,
+        email: "realalewales@gmail.com",
+        first_name: "Dave"
+        last_name: "Jones"
+        age: 58
+      },
+      {
+        id: 2,
+        email: "just-do-it@gmail.com",
+        first_name: "Shia"
+        last_name:"LaBoeuf"
+        age: 31
+      },
+    ]
   #Need to tell ERB to look for VIews (vivian) somewhere else not in this directory.
   # sets root as the parent-directory of the current file
     set :root, File.join(File.dirname(__FILE__), '..')
@@ -16,7 +46,7 @@ class UsersController < Sinatra::Base
 # require "sinatra"
 
   get "/" do
-    "<h1> Hello world! </h1>"
+    "Hello world!"
   end
 
 
